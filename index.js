7646bbda724e5d37bcd9488e1f339fda2aeae41b
Grayson Hicks <graysonhicks@gmail.com>
2016-04-18 17:53:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || 'parkaryHicks1988', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   restAPIKey: process.env.REST_API_KEY || '',
   liveQuery: {
