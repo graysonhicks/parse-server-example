dc4062a4a207f587ee9dc29f7c93301150077ccc
Grayson Hicks <graysonhicks@gmail.com>
2017-07-13 11:03:57 -0400

    db change test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var ParseServer = require("parse-server").ParseServer;
 var path = require("path");
 var S3Adapter = require("parse-server").S3Adapter;
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
 
 if (!databaseUri) {
 	console.log("DATABASE_URI not specified, falling back to localhost.");
